e846c46fc57eb24941a87fe3331b5c0c84f4e801
Motoharu Ito <63852487+Motoharu-Ito-for2030@users.noreply.github.com>
2020-07-20 09:33:43 +0900

    initial commit
    永遠です永遠です

---
+++ main.js
@@ -0,0 +1,56 @@
+'use strict'
+{
+
+  const btn = document.getElementById("btn")
+  btn.addEventListener('click', ()=> {
+    const results = ['大吉', '中吉', '凶','モトハル'];
+    // const n = Math.floor(Math.random() * results.length);
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
+  });
+// おみくじここまで
+// ここからビンゴ
+
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++){
+      source[i] = i + 1 + 15 * col;
+    }
+    const column = [];  
+    for (let i = 0; i < 5; i++){
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+    return column;
+  }
+
+  const columns =[];
+  columns[0] = createColumn(0); 
+  columns[1] = createColumn(1); 
+  columns[2] = createColumn(2); 
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(4); 
+  columns[2][2] = 'FREE';
+
+  
+// 表を横向きにする
+  const bingo = [];
+  for (let row = 0; row < 5; row++){
+    bingo[row] = [];
+    for (let col= 0; col < 5; col++){
+      bingo[row][col] = columns[col][row];
+    }
+  }
+ 
+// HTMLに移す
+
+
+ for(let row = 0; row < 5; row++){
+   const tr =document.createElement('tr');
+ 
+   for (let col = 0; col< 5; col++){
+     const td = document.createElement('td');
+     td.textContent = bingo[row][col];
+     tr.appendChild(td);
+   }
+   document.querySelector('tbody').appendChild(tr);
+ }
+}
